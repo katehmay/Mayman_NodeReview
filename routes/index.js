4f2962659973ea1bc15018de1b3c5251dd3378d3
Mayman <k_mayman@ats-hr420-dm11.fc.ca>
2019-10-31 12:54:16 -0400

    there's so much stuff omg

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,16 @@ router.get('/', (req, res) => {
         console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
+        //before we sent it thru
+        //map is an array method that lets you map one value to another (convert it)
+        result[0].social = result[0].social.split(".").map(function(item){
+            item = item.trim();
+            // item.trim() removes any empty white space from text
+
+            return item;
+        })
+
+        console.log("after trim / conversion:", result[0]);
         res.render('home', { people: result }); // data is all the details on the db, you can call it whatever you want
         //can be team, can be people, can be data; result can't be changed
     })
